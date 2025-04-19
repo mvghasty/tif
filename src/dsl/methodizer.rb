4ab3aa62b3a8756a981da57305fe062004ae73de
mvghasty <fasmagoric.dev@gmail.com>
2025-04-18 23:02:27 -0300

    Refactor let function and variables

--- src/dsl/methodizer.rb
+++ src/dsl/methodizer.rb
@@ -1,43 +1,90 @@
 module METHODIZER
   class STD
-    def self.cout(text)
-      $stdout.puts(text)
-    end
+    @@variables = {}
 
-    def for(init, condition, increment)
-      while condition.call
-        yield(init[:val])
-        init[:val] = increment.call(init[:val])
+    def self.cout(text)
+      text_str = text.to_s
+
+      begin
+        evaluated_text = self.instance_eval("\"#{text_str.gsub('"', '\"')}\"")
+        $stdout.puts(evaluated_text)
+      rescue NameError => e
+        puts "Erro na interpolação em cout '#{text_str}': Variável não encontrada - #{e.message}"
+      rescue SyntaxError => e
+         puts "Erro na interpolação em cout '#{text_str}': Erro de sintaxe - #{e.message}"
+      rescue Exception => e
+         puts "Erro ao avaliar a string de interpolação em cout '#{text_str}': #{e.message}"
       end
     end
 
-    @@variables = {}
+    def self.for(init, condition, increment, &block)
+       unless init.is_a?(Hash) && init.key?(:val)
+         raise "Formato de init inválido para o loop for. Esperado {:val => valor_inicial}"
+       end
+       unless condition.respond_to?(:call) && increment.respond_to?(:call) && block_given?
+         raise "Condition e increment no loop for devem ser callables (Proc ou Lambda), e um bloco deve ser fornecido."
+       end
+
+       while condition.call
+         block.call(init[:val])
+         init[:val] = increment.call(init[:val])
+       end
+    end
 
     def self.let(declaration)
       if declaration =~ /^(\w+):\s*&(\w+)\s*=\s*(.+)$/
-        name, type, value = $1, $2, $3
+        name, type, value_part_string = $1, $2, $3.strip
 
-        value = case type
-        when "int" then value.to_i
-        when "float" then value.to_f
-        when "string" then value.to_s
-        when "bool" then value == "true"
-        else value
+        final_value = nil
+
+        case type
+        when "str"
+          if value_part_string =~ /^".*"$/
+             begin
+               final_value = self.instance_eval(value_part_string)
+             rescue Exception => e
+               raise "Erro ao avaliar o valor string '#{value_part_string}' para '#{name}': #{e.message}"
+             end
+          else
+             final_value = value_part_string
+          end
+        when "int", "float", "bool"
+          begin
+            evaluated_value = self.instance_eval(value_part_string)
+
+            final_value = case type
+            when "int"    then evaluated_value.to_i
+            when "float"  then evaluated_f = evaluated_value.to_f; evaluated_f == evaluated_f.to_i ? evaluated_f : evaluated_f
+            when "bool"   then [true, "true", 1].include?(evaluated_value.to_s.downcase)
+            end
+          rescue NameError => e
+            raise "Variável não encontrada ou expressão inválida '#{value_part_string}' na declaração let para '#{name}': #{e.message}"
+          rescue SyntaxError => e
+             raise "Erro de sintaxe na expressão de valor '#{value_part_string}': #{e.message}"
+          rescue Exception => e
+             raise "Erro ao avaliar a expressão de valor '#{value_part_string}' para '#{name}': #{e.message}"
+          end
+        else
+          begin
+            final_value = self.instance_eval(value_part_string)
+          rescue Exception => e
+             raise "Erro ao avaliar a expressão de valor '#{value_part_string}' para '#{name}': #{e.message}"
+          end
         end
 
-        @@variables[name.to_sym] = value
-        Object.class_eval { define_method(name) { @@variables[name.to_sym] } }
+        @@variables[name.to_sym] = final_value
 
-        value
+        METHODIZER::STD.define_singleton_method(name.to_sym) { @@variables[name.to_sym] }
+
+
+        final_value 
       else
-        raise "Invalid let declaration format: #{declaration}"
+        raise "Formato de declaração let inválido: #{declaration}"
       end
     end
 
-    def self.get(name)
-      @@variables[name.to_sym]
+    def self.get(name_sym)
+      @@variables[name_sym]
     end
   end
 end
-
-@std = METHODIZER::STD
--- src/dsl/tsi.rb
+++ src/dsl/tsi.rb
@@ -1,25 +1,54 @@
 require_relative 'methodizer'
 
 class TSI
-  def main(&block)
-    std = METHODIZER::STD
-    std.instance_eval(&block)
+  module Fn
+    def self.main(&block)
+      std = METHODIZER::STD
+      std.instance_eval(&block)
+    end
   end
 
-  def preprocess_tff(code)
-    code.gsub(/^(\s*)let\s+([^"\n]+?)$/, '\1let "\2"')
+  def preprocess(code)
+    processed_lines = code.lines.map do |line|
+      stripped_line = line.strip
+      next '' if stripped_line.empty? || stripped_line.start_with?('#')
+
+      if stripped_line.start_with?('let ')
+        declaration_part = stripped_line.sub(/^let\s+/, '').strip
+        escaped_declaration_part = declaration_part.gsub('"', '\"')
+        "let \"#{escaped_declaration_part}\""
+      else
+        line
+      end
+    end
+    processed_lines.reject(&:empty?).join("\n")
   end
 
   def load_and_execute(filename)
     code = File.read(filename)
-    code = preprocess_tff(code)
-    instance_eval(code)
-  end
+    processed_code = preprocess(code)
+    instance_eval(processed_code)
+
+    rescue NameError => e
+      puts "Erro de execução da DSL (Variável/Método): Variável ou método '#{e.name}' não encontrado para #{e.receiver}"
+    rescue ArgumentError => e
+      puts "--- ArgumentError Capturado ---"
+      puts "Erro de execução da DSL (Argumentos): #{e.message}"
+      puts "Localização do erro (Stack Trace):"
+      puts e.backtrace.join("\n")
+      puts "-------------------------------"
+    rescue Exception => e
+      puts "--- Exception Capturada ---"
+      puts "Um erro inesperado ocorreu durante o processamento ou execução da DSL: #{e.message}"
+      puts "Localização do erro (Stack Trace):"
+      puts e.backtrace.join("\n")
+      puts "---------------------------"
+    end
 end
 
 if ARGV[0]
   interpreter = TSI.new
   interpreter.load_and_execute(ARGV[0])
 else
-  puts "Please provide the path of the .tff file as an argument." 
-end 
+  puts "Por favor, forneça o caminho do arquivo .tff como argumento."
+end
